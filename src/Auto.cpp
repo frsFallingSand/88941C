2ba233f8146f1c75961e90f8a1d30c19ae223088
qqxnkrut <frsfallingsand@outlook.com>
2025-10-24 20:23:16 +0800

    重构、提取函数、美化 1  与右侧镜像

--- src/Auto.cpp
+++ src/Auto.cpp
@@ -8,36 +8,86 @@ void stoplr(){
   R.stop(brake);
 }
 
-void Left(){
+void initcar(){
   Export.setStopping(hold);
   L.setStopping(brake);
   R.setStopping(brake);
+}
+
+void start2picked(bool r = 0){
+  // straight
   Intake.spin(forward,100,vex::velocityUnits::pct);
   Intake2.spin(forward,70,vex::velocityUnits::pct);
   MoveDistancePID(fwd,23,50,20,0,0.2,0.5);
-  R.spinFor(forward,1.1,turns,20,vex::velocityUnits::pct);
-  L.stop(brake);
-  R.stop(brake);
+
+  // turn
+  if (!r) R.spinFor(forward,1.1,turns,20,vex::velocityUnits::pct);
+  else L.spinFor(forward,1.1,turns,20,vex::velocityUnits::pct);
+  stoplr();
   wait(50,msec);
-  MoveDistancePID(fwd,16,15,10,336,0.2,0.5);
-  MoveDistancePID(reverse,2,15,10,336,0.2,-0.5);
-  smartTurn(225 , 0.47 , 0.05, 0.01);
-  MoveDistancePID(reverse,14.2,40,10,225,0.2,-0.5);
+
+  // pick
+  if (!r) {
+    MoveDistancePID(fwd,16,15,10,336,0.2,0.5);
+    MoveDistancePID(reverse,2,15,10,336,0.2,-0.5);
+  } else {
+    MoveDistancePID(fwd,16,15,10,24,0.2,0.5);
+    MoveDistancePID(reverse,2,15,10,24,0.2,-0.5);
+  }
+}
+
+void picked2centered(bool r = 0){
+  // face at
+  if (!r) smartTurn(225 , 0.47 , 0.05, 0.01);
+  else smartTurn(315 , 0.47 , 0.05, 0.01);
+  if (!r) MoveDistancePID(reverse,14.2,40,10,225,0.2,-0.5);
+  else MoveDistancePID(fwd,14.2,40,10,315,0.2,-0.5);
 
   //退后顶中桥吐球
   L.spin(reverse,8,vex::velocityUnits::pct);
   R.spin(reverse,8,vex::velocityUnits::pct);
-  Intake2.stop();//关闭intake2，防止多余球入中桥
-  Export.spin(reverse,70,vex::velocityUnits::pct);
-  L.stop(brake);
-  R.stop(brake);
+  if (!r) {
+    Intake2.stop();//关闭intake2，防止多余球入中桥
+    Export.spin(reverse,70,vex::velocityUnits::pct);
+  } else {
+    Intake2.spin(reverse,70,vex::velocityUnits::pct);
+    wait(500,msec);
+    Intake.spin(reverse,30,vex::velocityUnits::pct);
+  }
+  stoplr();
   wait(500,msec);
   Export.stop();
-  MoveDistancePID(fwd,49.5,50,15,225,0.2,0.5); //中到桶
+  Intake.stop();
+  Intake2.stop();
+}
+
+void centered2sided(bool r = 0){
+  if (!r) MoveDistancePID(fwd,49.5,50,15,225,0.2,0.5);
+  else MoveDistancePID(fwd,49.5,50,15,135,0.2,0.5);
   smartTurn(180, 0.48 , 0.05 , 0.16);
   Bucket_to_Bridge();
+}
+
+void knock_bucket(){
   move(fwd,300,100);
   moveTime(reverse,100,400);
+}
+
+void Left(){
+  initcar();
+  start2picked();
+  picked2centered();
+  centered2sided();
+  knock_bucket();
+}
+
+void Right(){
+  initcar();
+  start2picked(1);
+  picked2centered(1);
+  return;
+  centered2sided(1);
+  knock_bucket();
 }
 
 void Auto(){
@@ -107,6 +157,3 @@ void Auto(){
 }
 
 
-void Right(){
-  return;
-}
