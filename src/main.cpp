b79e15e8d42e5d2b8eda9539e9965eeabf7a5922
qqxnkrut <frsfallingsand@outlook.com>
2025-10-27 21:41:54 +0800

    稳定性测试1 不太行;开写技能！

--- src/main.cpp
+++ src/main.cpp
@@ -151,7 +151,7 @@ int main() {
     wait(50, msec);
   }
   thread Inertialinit(init);
-  Competition.autonomous(Right);
+  Competition.autonomous(Auto);
   Competition.drivercontrol(usercontrol);
   // if (t){
   //   test();
