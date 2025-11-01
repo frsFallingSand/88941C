0715d4ddb4bfb7702e2beb3447a226393520bf93
qqxnkrut <frsfallingsand@outlook.com>
2025-11-01 20:09:15 +0800

    skill2-1

--- src/Auto.cpp
+++ src/Auto.cpp
@@ -1,180 +1,294 @@
 #include "vex.h"
 
-/**
- * 左侧自动程序（从场地左侧出发）
- */
-void stoplr() {
+
+
+void Left(){
+
+  
+  Intake.spin(forward,100,vex::velocityUnits::pct);
+  Intake2.spin(forward,70,vex::velocityUnits::pct);
+  //Export.spin(reverse,70,vex::velocityUnits::pct);
+  MoveDistancePID(fwd,11,30,20,0,0.2,0.2);
+  R.spinFor(forward,1.2,turns,20,vex::velocityUnits::pct);
   L.stop(brake);
   R.stop(brake);
-}
-
-void initcar() {
-  Export.setStopping(hold);
-  L.setStopping(brake);
-  R.setStopping(brake);
-}
-
-void start2picked(bool r = 0) {
-  // straight
-  Intake.spin(forward, 100, vex::velocityUnits::pct);
-  Intake2.spin(forward, 70, vex::velocityUnits::pct);
-  MoveDistancePID(fwd, 23, 50, 20, 0, 0.2, 0.5); //23 8.8
-
-  // turn
-  if (!r)
-    R.spinFor(forward, 1.1, turns, 20, vex::velocityUnits::pct);
-  else
-    L.spinFor(forward, 1.3, turns, 80, vex::velocityUnits::pct);
-  stoplr();
-  wait(50, msec);
-
-  // pick
-  if (!r) {
-    MoveDistancePID(fwd, 16, 15, 10, 336, 0.2, 0.5);
-    MoveDistancePID(reverse, 2, 15, 10, 336, 0.2, -0.5);
-  } else {
-    MoveDistancePID(fwd, 16, 15, 10, 24, 0.2, 0.5);
-    MoveDistancePID(reverse, 2.7, 15, 10, 24, 0.2, -0.5);
-  }
-}
-
-void picked2centered(bool r = 0) {
-  // face at
-  if (!r)
-    smartTurn(225, 0.47, 0.05, 0.01);
-  else
-    smartTurn(315, 0.47, 0.05, 0.01);
-  if (!r)
-    MoveDistancePID(reverse, 14.2, 40, 10, 225, 0.2, -0.5);
-  else
-    MoveDistancePID(fwd, 14, 40, 10, 315, 0.4, 0.3);
-
-  // 退后顶中桥吐球
-  if (!r) {
-    L.spin(reverse, 8, vex::velocityUnits::pct);
-    R.spin(reverse, 8, vex::velocityUnits::pct);
-  } else {
-    // L.spin(fwd,8,vex::velocityUnits::pct);
-    // R.spin(fwd,8,vex::velocityUnits::pct);
-  }
-  if (!r) {
-    Intake2.stop(); // 关闭intake2，防止多余球入中桥
-    Export.spin(reverse, 70, vex::velocityUnits::pct);
-  } else {
-    Intake2.spin(reverse, 100, vex::velocityUnits::pct);
-    wait(500, msec);
-    Intake.spin(reverse, 30, vex::velocityUnits::pct);
-    wait(300, msec);
-    Intake.spin(reverse, 10, vex::velocityUnits::pct);
-  }
-  stoplr();
-  wait(500, msec);
+  wait(50,msec);
+  MoveDistancePID(fwd,14,12,12,331,0.2,0.2);
+  MoveDistancePID(reverse,3.5,30,10,331,0.2,-0.2);
+  smartTurn(224 , 0.48 , 0.07, 0.01);
+  MoveDistancePID(reverse,13.5,40,10,224,0.2,-0.3);
+  Intake2.stop();//关闭intake2，防止多余球入中桥
+  Export.spin(reverse,90,vex::velocityUnits::pct);
+  //退后顶中桥吐球
+  L.spin(reverse,5,vex::velocityUnits::pct);
+  R.spin(reverse,5,vex::velocityUnits::pct);
+  wait(250,msec);
   Export.stop();
-  Intake.stop();
-  Intake2.stop();
-  if (r)
-    wait(500, msec);
-}
-
-void centered2sided(bool r = 0) {
-  if (!r)
-    MoveDistancePID(fwd, 49.5, 50, 15, 225, 0.2, 0.5);
-  else {
-    Intake.spin(forward, 20, vex::velocityUnits::pct);
-    L.spinFor(reverse, 0.3183098862, turns, 80, vex::velocityUnits::pct);
-    R.spinFor(reverse, 0.3183098862, turns, 80, vex::velocityUnits::pct);
-    // MoveDistancePID(reverse, 2, 50, 50, 315, 0.2, 0.7);
-    // Intake2.spin(forward, 70, vex::velocityUnits::pct);
-    // wait(200, msec);
-    MoveDistancePID(reverse, 45, 50, 15, 315, 0.2, 0.3);
-    // wait(500, msec);
-  }
-  smartTurn(180, 0.48, 0.05, 0.16);
-  if (!r)
-    Bucket_to_Bridge();
-  else
-    Bucket_to_Bridge_rdiff();
-}
-
-void knock_bucket() {
-  move(fwd, 300, 100);
-  moveTime(reverse, 100, 400);
-}
-
-void Left() {
-  initcar();
-  start2picked();
-  picked2centered();
-  centered2sided();
-  knock_bucket();
-}
-
-void Right() {
-  initcar();
-  start2picked(1);
-  picked2centered(1);
-  centered2sided(1);
-  knock_bucket();
-}
-
-void go_straight() {
-  Intake.spin(forward, 100, vex::velocityUnits::pct);
-  Intake2.spin(forward, 60, vex::velocityUnits::pct);
-  // MoveDistancePID(fwd, 23, 50, 20, 0, 0.2, 0.5);
-  // R.spinFor(forward, 1.1, turns, 20, vex::velocityUnits::pct);
-  MoveDistancePID(fwd, 39, 60, 60, 345, 0.2, 0.5);
-  MoveDistancePID(fwd, 60, 60, 50, 15, 0.2, 0.5);
-  Intake.stop();
-  Intake2.stop();
-}
-
-void rotation_to_correct(vex::directionType dir, float Speed, float targetAngle, float deltaDeg) {
-  int deg_error = targetAngle - IMU.heading();
-  while(deg_error >= deltaDeg) {
-    deg_error = targetAngle - IMU.heading();
-    L.spin(dir, Speed, pct);
-    R.spin(dir, Speed, pct);
-  }
-  stoplr();
-}
-
-void go_side() {
-  smartTurn(55, 0.47, 0.05, 0.01);
-  // MoveDistancePID(reverse, 33, 30, 10, 55, 0.2, 0.4);
-  rotation_to_correct(reverse, 30, 0, 2);
-  return;
+  L.stop(brake);
+  R.stop(brake);
+  MoveDistancePID(fwd,49,55,15,226,0.2,0.4);
+  smartTurn(180 , 0.48 , 0.05 , 0.16);
   Bucket_to_Bridge();
+  DigitalOutF.set(true);
+  Occupying_the_scoring_zone();
+  L.stop();
+  R.stop();
 }
 
-void only_bridge(){
-  moveTime(reverse, 50, 1200);
-  Export.spin(fwd, 100, vex::velocityUnits::pct);
-  Intake.spin(forward, 100, vex::velocityUnits::pct);
-  Intake2.spin(forward, 100, vex::velocityUnits::pct);
-  for (int i = 0; i < 4; i++) {
-    L.spinFor(fwd, 50, deg, 100, vex::velocityUnits::pct, false);
-    R.spinFor(reverse, 90, deg, 100, vex::velocityUnits::pct);
-    L.spinFor(reverse, 90, deg, 100, vex::velocityUnits::pct, false);
-    R.spinFor(fwd, 50, deg, 100, vex::velocityUnits::pct);
-  }
-  moveTime(reverse, 100, 400);
-  wait(0.6, sec);
+
+void Right(){
+  
+  Intake.spin(forward,100,vex::velocityUnits::pct);
+  Intake2.spin(forward,70,vex::velocityUnits::pct);
+  //Export.spin(reverse,70,vex::velocityUnits::pct);
+  MoveDistancePID(fwd,11,30,20,0,0.2,0.2);
+  L.spinFor(forward,1.2,turns,20,vex::velocityUnits::pct);
+  L.stop(brake);
+  R.stop(brake);
+  wait(50,msec);
+  MoveDistancePID(fwd,14,12,12,29,0.2,0.2);
+  MoveDistancePID(reverse,6,30,10,29,0.2,-0.2);
+  Intake.setStopping(hold);
+  Intake2.spin(reverse,100,vex::velocityUnits::pct);
+  smartTurn(-46 , 0.48 , 0.07, 0.01);
+  MoveDistancePID(fwd,11,40,10,-46,0.2,0.3);
+  L.stop(brake);
+  R.stop(brake);
+  Intake2.stop();
+  Intake.spinFor(reverse,200,deg,30,vex::velocityUnits::pct);
+  Intake2.stop();//关闭intake2，防止多余球入中桥
+  Intake.setStopping(coast);
+  //退后顶中桥吐球
+  MoveDistancePID(reverse,42,55,15,-46,0.2,-0.4);
+  
+  smartTurn(180 , 0.48 , 0.05 , 0.16);
+  Intake.spin(forward,100,vex::velocityUnits::pct);
+  Intake2.spin(forward,70,vex::velocityUnits::pct);
+  Bucket_to_Bridge();
+  DigitalOutF.set(true);
+  Occupying_the_scoring_zone();
+  L.stop();
+  R.stop();
+}
+
+
+void intakefuntion(){
+  Intake.spin(forward,100,vex::velocityUnits::pct);
+  Intake2.spin(forward,100,vex::velocityUnits::pct);
+}
+
+void intakestop(){
   Intake.stop();
   Intake2.stop();
   Export.stop();
 }
 
-void picked2sided(){
-  smartTurn(225, 0.47, 0.05, 0.01);
-  MoveDistancePID(fwd, 35.3, 50, 15, 225, 0.2, 0.5);
-  smartTurn(180, 0.47, 0.05, 0.01);
-  only_bridge();
+void intake_import(){
+  Intake.spin(forward,100,vex::velocityUnits::pct);
+  Intake2.spin(forward,70,vex::velocityUnits::pct);
+  Export.spin(forward,100,vex::velocityUnits::pct);
 }
 
-void Auto() {
-  initcar();
-  start2picked();
-  picked2sided();
-  // go_straight();
-  // go_side();
+
+
+
+
+void skill(){
+
+  while(IMU.isCalibrating()){
+    Brain.Screen.setCursor(5,5);
+    Controller1.Screen.setCursor(2,4);
+    Controller1.Screen.print("Inertial is calibrate");
+    Brain.Screen.print("Inertial is calibrate");
+    wait(50, msec);
+  }
+  wait(1,sec);
+  
+  Intake.spin(forward,100,vex::velocityUnits::pct);
+  Intake2.spin(forward,60,vex::velocityUnits::pct);
+  MoveDistancePID(fwd,10.5,35,20,0,0.2,0.2);
+  R.spinFor(forward,1.2,turns,40,vex::velocityUnits::pct);
+  L.stop(brake);
+  R.stop(brake);
+  wait(50,msec);
+  MoveDistancePID(fwd,14,30,20,333,0.2,0.2);
+  wait(50,msec);
+  MoveDistancePID(reverse,3.5,30,10,333,0.2,-0.2);
+  //指向1桶
+  smartTurn(225 , 0.49 , 0.019, 0.01);
+  MoveDistancePID(fwd,34.5,55,20,225,0.2,0.4);
+  wait(50,msec);
+  intakestop();
+  smartTurn(180 , 0.49 , 0.019 , 0.01);
+
+
+  front_panel.set(true);
+  //Intake2.spin(reverse,100,pct);
+  wait(50,msec);
+  //Intake2.stop();
+  intakefuntion();
+  moveTime(fwd,40,500);
+  L.spin(fwd,2,pct);
+  R.spin(fwd,2,pct);
+  wait(1.3,sec);
+  L.stop(brake);
+  R.stop(brake);
+  wait(0.2,sec);
+  MoveDistancePID(reverse,26,40,40,180,0.2,-0.4);
+  Intake2.spin(reverse,100,pct);
+  moveTime(reverse,70,200);
+  intakestop();
+  intake_import();
+  for(int i=0;i<2;i++){
+    L.spinFor(fwd,40,deg,100,vex::velocityUnits::pct,false);
+    R.spinFor(reverse,50,deg,100,vex::velocityUnits::pct);
+    L.spinFor(reverse,50,deg,100,vex::velocityUnits::pct,false);
+    R.spinFor(fwd,40,deg,100,vex::velocityUnits::pct);
+  }
+  
+  L.spin(reverse,100,pct);
+  R.spin(reverse,100,pct);
+  wait(2.2,sec);
+  move(fwd,200,30);
+  wait(100,msec);
+  front_panel.set(false);
+  moveTime(reverse,20,400);
+  wait(100,msec);
+
+
+  MoveDistancePID(fwd,15,50,8,180,0.2,-0.5);
+  wait(50,msec);
+  L.spinFor(reverse,600,deg,40,vex::velocityUnits::pct);
+  L.stop(brake);
+  R.stop(brake);
+  wait(50,msec);
+  L.spinFor(reverse,450,deg,40,vex::velocityUnits::pct,false);
+  R.spinFor(reverse,1000,deg,40,vex::velocityUnits::pct);
+  intakestop();
+  MoveDistancePID(reverse,74,80,15,180,0.2,-0.5);
+  R.spinFor(fwd,1400,deg,40,vex::velocityUnits::pct);
+  //靠墙定位
+  moveTime(reverse,40,700);
+  wait(200,msec);
+  IMU.setHeading(0,deg);
+  wait(100,msec);
+  MoveDistancePID(fwd,14,40,15,0,0.2,0.5);
+  smartTurn(-90 , 0.48 , 0.018 , 0.01);
+
+  // //拿2桶
+  front_panel.set(true);
+  wait(100,msec);
+  intakefuntion();
+  MoveDistancePID(fwd,10,40,20,270,0.2,0.2);
+  moveTime(fwd,15,800);
+  wait(1.3,sec);
+  intakestop();
+  front_panel.set(false);
+  MoveDistancePID(reverse,9,40,20,270,0.2,-0.2);
+  smartTurn(45 , 0.48 , 0.018, 0.01);
+  intakefuntion();
+  MoveDistancePID(fwd,32,50,30,45,0.2,0.3);
+  smartTurn(0 , 0.48 , 0.018, 0.01);
+  MoveDistancePID(fwd,38.5,70,30,0,0.2,0.3);
+  smartTurn(315 , 0.48 , 0.018, 0.01);
+  MoveDistancePID(reverse,8,40,20,315,0.2,-0.2);
+  Intake2.spin(reverse,100,vex::velocityUnits::pct);
+  moveTime(reverse,20,600);
+  //导入中桥
+  intakefuntion();
+  Export.spin(reverse,40,vex::velocityUnits::pct);
+  wait(1,sec);
+  Export.spin(reverse,30,vex::velocityUnits::pct);
+  wait(1.5,sec);
+  wait(200,msec);
+
+  R.spinFor(fwd,1600,deg,80,vex::velocityUnits::pct,false);
+  L.spinFor(fwd,2200,deg,80,vex::velocityUnits::pct);  
+  intakestop();
+  moveTime(fwd,40,1200);
+  wait(100,msec);
+  IMU.setHeading(0,deg);
+  wait(100,msec);
+
+  //去3桶位置
+  MoveDistancePID(reverse,9,55,15,0,0.2,-0.4);
+  smartTurn(270 , 0.47 , 0.018 , 0.16);
+  front_panel.set(true);
+  wait(100,msec);
+  intakefuntion();
+  moveTime(fwd,40,700);
+  // L.spin(fwd,2,pct);
+  // R.spin(fwd,2,pct);
+  wait(1.2,sec);
+  L.stop(brake);
+  R.stop(brake);
+  wait(0.2,sec);
+  MoveDistancePID(reverse,5,30,15,270,0.2,-0.5);
+
+  front_panel.set(false);
+  wait(100,msec);
+  
+  L.spinFor(reverse,450,deg,40,vex::velocityUnits::pct);
+  wait(100,msec);
+  L.spinFor(reverse,700,deg,40,vex::velocityUnits::pct,false);
+  R.spinFor(reverse,1200,deg,40,vex::velocityUnits::pct);  
+  intakestop();
+
+  MoveDistancePID(reverse,76,80,15,270,0.2,-0.5);
+  R.spinFor(fwd,1000,deg,40,vex::velocityUnits::pct);
+  // //靠墙定位
+  moveTime(reverse,40,700);
+  wait(100,msec);
+  IMU.setHeading(0,deg);
+  wait(100,msec);
+  //四桶
+
+  MoveDistancePID(fwd,12,30,15,0,0.2,0.5);
+  smartTurn(270 , 0.49 , 0.02 , 0.16);
+  moveTime(reverse,60,700);
+  intake_import();
+  for(int i=0;i<2;i++){
+    L.spinFor(fwd,40,deg,100,vex::velocityUnits::pct,false);
+    R.spinFor(reverse,50,deg,100,vex::velocityUnits::pct);
+    L.spinFor(reverse,50,deg,100,vex::velocityUnits::pct,false);
+    R.spinFor(fwd,40,deg,100,vex::velocityUnits::pct);
+  }
+  L.spin(reverse,100,vex::velocityUnits::pct);
+  R.spin(reverse,100,vex::velocityUnits::pct);
+  wait(1.2,sec);
+  L.stop();
+  R.stop();
+
+  intakestop();
+  front_panel.set(true);
+  intakefuntion();
+  MoveDistancePID(fwd,24,50,20,270,0.2,0.4);
+  moveTime(fwd,20,600);
+  wait(0.9,sec);
+
+  MoveDistancePID(reverse,26,40,20,270,0.2,-0.4);
+  Intake2.spin(reverse,100,pct);
+  moveTime(reverse,70,200);
+  intakestop();
+  intake_import();
+  for(int i=0;i<2;i++){
+    L.spinFor(fwd,40,deg,100,vex::velocityUnits::pct,false);
+    R.spinFor(reverse,50,deg,100,vex::velocityUnits::pct);
+    L.spinFor(reverse,50,deg,100,vex::velocityUnits::pct,false);
+    R.spinFor(fwd,40,deg,100,vex::velocityUnits::pct);
+  }
+  DigitalOutF.set(true);
+  front_panel.set(false);
+  L.spin(reverse,100,pct);
+  R.spin(reverse,100,pct);
+  wait(2,sec);
+  
+  L.spin(fwd,60,pct);
+  R.spin(fwd,40,pct);
+  wait(1.5,sec);
+  L.spin(fwd,100,pct);
+  R.spin(fwd,100,pct);
+  wait(0.5,sec);
+  L.stop();
+  R.stop();
 }
